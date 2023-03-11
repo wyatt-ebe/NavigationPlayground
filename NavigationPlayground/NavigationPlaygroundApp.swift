5c94a8d134ec1464d29420d8c7ef68d96e08bb9c
Wyatt Eberspacher <wseberspacher@gmail.com>
2023-03-11 13:56:26 -0800

    Initial Commit

---
+++ NavigationPlayground/NavigationPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPlaygroundApp.swift
+//  NavigationPlayground
+//
+//  Created by Wyatt on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
